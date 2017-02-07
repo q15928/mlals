c9f12498fa98295de939944f64b14fb372f190db
Jason Feng <Jason.Feng@vocus.com.au>
2017-02-07 11:16:37 +1100

    correct typo in als/__init__

--- als/__init__.py
+++ als/__init__.py
@@ -1,4 +1,4 @@
-from .navie_bayes import MultinomialNBClassifier
+from .naive_bayes import MultinomialNBClassifier
 from .knn_classifier import KNNClassifier
 from .linear_regression import LinearRegression
 from .logistic_regression import LogisticRegression
